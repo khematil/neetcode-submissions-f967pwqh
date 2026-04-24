352c990b739d0b9082888d5b0099c45edc9fb907
Kyle H <46408128+khematil@users.noreply.github.com>
2026-04-24 16:44:40 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        left = 0
+        s1Map = {}
+        s2Map = {}
+
+        for char in s1: 
+            s1Map[char] = s1Map.get(char, 0) + 1
+        
+        for right in range(len(s2)):
+            s2Map[s2[right]] = s2Map.get(s2[right], 0) + 1
+
+            # condition if window size > length of s1 we increase left pointer
+            while (right - left + 1) > len(s1):
+                s2Map[s2[left]] -= 1
+
+                if s2Map[s2[left]] == 0:
+                    del s2Map[s2[left]]
+
+                left += 1
+            
+            print(s1Map)
+            print(s2Map)
+            if s1Map == s2Map:
+                return True
+
+        return False
+
+
